e8ecb1c9386e1fbfa7263fad5b61085851279cb3
lindenb4rk <maksim.lindenbark@gmail.com>
2025-01-30 18:06:33 +0300

    Update insert_procedures.sql

--- insert_procedures.sql
+++ insert_procedures.sql
@@ -8,8 +8,7 @@ FROM 'D:\deal_info.csv'
 WITH (FORMAT CSV, DELIMITER ',', HEADER, ENCODING 'WIN1251');
 
 GET DIAGNOSTICS lines = ROW_COUNT;
-
-PERFORM logs_insert('Copy to deal_info from file:'||lines||' lines');
+CALL logs_insert('Copy to deal_info from file:'||lines||' lines');
 
 END $$;
 
@@ -20,14 +19,15 @@ OR REPLACE PROCEDURE preINSERT_VALUES_2 () LANGUAGE PLPGSQL AS $$
 DECLARE 
 lines bigint;
 BEGIN
+truncate rd.product_v2;
 
 COPY rd.product_v2
 FROM 'D:\product_info.csv'
 WITH (FORMAT CSV, DELIMITER ',', HEADER, ENCODING 'WIN1251');
 
---логи
+--логи,сколько поступило значений
 GET DIAGNOSTICS lines = ROW_COUNT;
-PERFORM logs_insert('Copy to product_v2(temp table) from file:'||lines||' lines');
+CALL logs_insert('Copy to product_v2(temp table) from file:'||lines||' lines');
 
 
 END $$;
@@ -44,25 +44,26 @@ lines bigint;
 BEGIN
 --удаляем изменённые в основной таблице, наши "входящие" значения, используя ключ
 
-with updates as (UPDATE rd.product as pr
-set effective_from_date =  pr2.effective_from_date
+WITH updates as (UPDATE rd.product as pr
+	SET effective_from_date =  pr2.effective_from_date
 --можно from заменить IN в WHERE 
-from rd.product_v2 pr2 
-where pr2.product_rk = pr.product_rk and pr2.product_name = pr.product_name  and pr.effective_from_date < pr2.effective_from_date
+FROM rd.product_v2 pr2 
+	WHERE pr2.product_rk = pr.product_rk and pr2.product_name = pr.product_name  and pr.effective_from_date < pr2.effective_from_date
 RETURNING pr.product_rk,pr.product_name)
 
 --происходит изменение строк и удаление их из вставляемой таблицы
 DELETE FROM rd.product_v2 
-WHERE (product_rk,product_name) IN (updates);
+WHERE (product_rk,product_name) IN (select * from updates);
 
---логи
+--логи,сколько удалили изменённых
 GET DIAGNOSTICS lines = ROW_COUNT;
-PERFORM logs_insert('DELETE FROM product_v2:'||lines||' lines');
+CALL logs_insert('DELETE FROM product_v2:'||lines||' lines');
 
-insert into rd.product select * from rd.product_v2;
+INSERT INTO rd.product SELECT * FROM rd.product_v2;
 
+--логи,сколько вставили значений
 GET DIAGNOSTICS lines = ROW_COUNT;
-PERFORM logs_insert('INSERT ONTO product:'||lines||' lines');
+CALL logs_insert('INSERT ONTO product:'||lines||' lines');
 
 truncate rd.product_v2;
 
