3f02d24994840774bb06051f07c9c42fa4792da5
lindenb4rk <maksim.lindenbark@gmail.com>
2025-01-30 18:06:06 +0300

    Add files via upload

--- steps_answer.sql
+++ steps_answer.sql
@@ -7,8 +7,24 @@ SELECT deal_rk, deal_num, deal_name, deal_sum, client_rk, account_rk, agreement_
 	FROM rd.deal_info*/
 
 
+	/*
+
+	SELECT 
+	--по идее: ключ
+	di.deal_rk, di.deal_num, di.deal_name,
+	di2.deal_rk, di2.deal_num, di2.deal_name,
+	--сравнение остальных значений
+	di.deal_sum, di.client_rk, di.account_rk, di.agreement_rk, di.deal_start_date, di.department_rk, di.product_rk, di.deal_type_cd,--, effective_from_date, effective_to_date
+		di2.deal_sum, di2.client_rk, di2.account_rk, di2.agreement_rk, di2.deal_start_date, di2.department_rk, di2.product_rk, di2.deal_type_cd--, effective_from_date, effective_to_date
+	FROM rd.deal_info as di
+	join rd.deal_info_v2 as di2 on di.deal_rk = di2.deal_rk
+	*/
+
+
 
 --SELECT * FROM logs.logs_info;
+
+--select * from rd.deal_info;
 
 --deal_info
 call insert_values();
